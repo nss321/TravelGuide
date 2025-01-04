94feed64d40659237f64c40663180d5efee0fae3
BAE <lexon1209@gmail.com>
2025-01-04 17:23:05 +0900

    init: TableViewController 추가, Model 추가, FirstTableViewController 레이아웃 가이드

---
+++ TravelGuide/FirstTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FirstTableViewController.swift
+//  TravelGuide
+//
+//  Created by BAE on 1/4/25.
+//
+
+import UIKit
+
+class FirstTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(self)
+        print(Int.max)
+    }
+
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 0
+    }
+
+   
+
+}
