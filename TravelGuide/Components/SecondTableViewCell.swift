cc50f635524c29556fe40e5b328310c1bea7040d
BAE <lexon1209@gmail.com>
2025-01-04 17:27:21 +0900

    Chore: CustonCell 추가

---
+++ TravelGuide/Components/SecondTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SecondTableViewCell.swift
+//  TravelGuide
+//
+//  Created by BAE on 1/4/25.
+//
+
+import UIKit
+
+class SecondTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TravelGuide/Components/ThirdTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ThirdTableViewCell.swift
+//  TravelGuide
+//
+//  Created by BAE on 1/4/25.
+//
+
+import UIKit
+
+class ThirdTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
