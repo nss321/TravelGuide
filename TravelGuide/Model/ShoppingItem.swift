86b9bc84d557d70540825ad3b851921cf55118a1
BAE <lexon1209@gmail.com>
2025-01-05 16:38:11 +0900

    Feat: ThirdTableView Model 추가

---
+++ TravelGuide/Model/ShoppingItem.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingItem.swift
+//  TravelGuide
+//
+//  Created by BAE on 1/5/25.
+//
+
+import Foundation
+  
+struct ShoppingItem {
+    var isChecked: Bool
+    let item: String
+    var isStarred: Bool
+}
+//
+//struct ShoppingItemDummy {
+//    let list: [ShoppingItem] = [
+//        ShoppingItem(isChecked: false, item: "바나나", isStarred: false)
+//    ]
+//}
